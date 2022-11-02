954672d5d676d93b97f395ed4997ea107b8d715d
Utkir <utkirkurbanov2700@gmail.com>
2022-11-02 11:10:58 +0500

    1-st commit

---
+++ code.js
@@ -0,0 +1,74 @@
+// function myFunction() {
+//     alert('Hello!');
+//     console.log('Hello!');
+//     document.write('Hello!')
+// }
+
+//Variables
+
+// function myFunction() {
+//     var number1 = 1;
+//     let number2 = 2;
+//     let number3 = 3;
+
+//     alert(number1 + number2);
+// }
+
+// function myFunction() {
+//     var priceItem = 300;
+//     var userPayment = 250;
+
+//     if(priceItem == userPayment) {
+//         alert('Payment successful!');
+//     } else {
+//         alert('Payment failed');
+//     }
+// }
+
+//Arrays & Strings
+
+
+
+
+
+
+
+// function myFunction() {
+
+//     var text = 'Hello!';
+//     const products = ['computer', 'phone', 'laptop'];
+//     products[0] = 'desktop computer';
+//     products.push('keyboard');
+//     alert(products.length);
+//     alert(products[products.length-1]);
+//     alert(products);
+//     alert(text.length);
+// }
+
+
+// function myFunction() {
+//     const products = ['computer', 'phone', 'laptop'];
+//     products.push('keyboard');
+
+//     for (let i = 0; i < products.length; i++) {
+//         console.log(products[i]);
+//     }
+
+//     for(let x in products) {
+//         console.log(products[x]);
+//     }
+
+//     for(let x of products) {
+//         console.log(x);
+//     }
+
+//     var i = 0;
+//     while(i < products.length) {
+//         console.log(products[i]);
+//         i++;
+//     }
+// }
+
+function myFunction(form) {
+    alert(form.inputbox.value);
+}
